d22a10fa8b7aece4725826ba848c9613e7e1018b
Erica Wright <ericaw21@gmail.com>
2017-02-02 23:56:44 -0800

    add navbar menu to switch colors for layout background

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,32 @@
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function() {
   $(".nomenuview").click(function() {
     $(".menuview").toggle();
+  });
+  $(".button-collapse").sideNav();
+
+  $("#blue").click(function() {
+    $("body").css("background-color", "#4D79A8");
+    $(".nav-wrapper").css("background-color", "#4D79A8");
+    $("#mobile-demo").css("background-color", "#4D79A8");
+  });
+  $("#red").click(function() {
+    $("body").css("background-color", "#E2BBC1");
+    $(".nav-wrapper").css("background-color", "#E2BBC1");
+    $("#mobile-demo").css("background-color", "#E2BBC1");
+  });
+  $("#green").click(function() {
+    $("body").css("background-color", "#5BB280");
+    $(".nav-wrapper").css("background-color", "#5BB280");
+    $("#mobile-demo").css("background-color", "#5BB280");
   });
 });
