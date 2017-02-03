ea3e4c198602c34758d8513c6d4153e8a81875a8
Erica Wright <ericaw21@gmail.com>
2017-02-03 11:48:54 -0800

    change navbar and add color buttons to toggle background colors

--- js/scripts.js
+++ js/scripts.js
@@ -12,21 +12,25 @@ $(document).ready(function() {
   $(".nomenuview").click(function() {
     $(".menuview").toggle();
   });
-  $(".button-collapse").sideNav();
+
+
 
   $("#blue").click(function() {
     $("body").css("background-color", "#4D79A8");
-    $(".nav-wrapper").css("background-color", "#4D79A8");
-    $("#mobile-demo").css("background-color", "#4D79A8");
+    $("#navbar").css("background-color", "#4D79A8");
+    $(".navbar-toggle").css("background-color", "#4D79A8");
+    $("figure").css("background-color", "#4D79A8");
   });
   $("#red").click(function() {
     $("body").css("background-color", "#E2BBC1");
-    $(".nav-wrapper").css("background-color", "#E2BBC1");
-    $("#mobile-demo").css("background-color", "#E2BBC1");
+    $("#navbar").css("background-color", "#E2BBC1");
+    $(".navbar-toggle").css("background-color", "#E2BBC1");
+    $("figure").css("background-color", "#E2BBC1");
   });
   $("#green").click(function() {
     $("body").css("background-color", "#5BB280");
-    $(".nav-wrapper").css("background-color", "#5BB280");
-    $("#mobile-demo").css("background-color", "#5BB280");
+    $("#navbar").css("background-color", "#5BB280");
+    $(".navbar-toggle").css("background-color", "#5BB280");
+    $("figure").css("background-color", "#5BB280");
   });
 });
